85f1c1df3bfdb39af4cbea5af1a5954383d838db
Tran Dinh Hung 20170078 <hung.td170078@sis.hust.edu.vn>
2020-05-07 17:51:34 +0700

    deploy rasa 1.7

---
+++ actions_app/actions.py
@@ -0,0 +1,63 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+import webbrowser
+import urllib.request
+from bs4 import BeautifulSoup
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionPlayMusic(Action):
+    
+    def name(self) -> Text:
+        return "action_play_music"
+    
+    def url_search(self, search_string, max_search):
+        textToSearch = search_string
+        query = urllib.parse.quote(textToSearch)
+        url = "https://www.youtube.com/results?search_query=" + query
+        response = urllib.request.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        vid = soup.findAll(attrs={'class':'yt-uix-tile-link'})[0]
+        url = 'https://www.youtube.com' + vid['href']          
+        return url
+    
+    def run(self, dispatcher, tracker, domain):
+        
+        song = tracker.get_slot('song')
+        query = urllib.parse.quote(song)
+        url = "https://www.youtube.com/results?search_query=" + query
+        response = urllib.request.urlopen(url)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        vid = soup.findAll(attrs={'class':'yt-uix-tile-link'})[0]
+        url = 'https://www.youtube.com' + vid['href']
+        webbrowser.open(url)
+        
+        dispatcher.utter_message(text="Here your are.")
+        return []
+    
+    
---
+++ data/nlu.md
@@ -0,0 +1,57 @@
+## intent:greet
+- hey
+- hello
+- hi
+- good morning
+- good evening
+- hey there
+
+## intent:goodbye
+- bye
+- goodbye
+- see you around
+- see you later
+
+
+## intent:mood_great
+- perfect
+- very good
+- great
+- amazing
+- wonderful
+- I am feeling very good
+- I am great
+- I'm good
+
+## intent:mood_unhappy
+- sad
+- very sad
+- unhappy
+- bad
+- very bad
+- awful
+- terrible
+- not very good
+- extremely sad
+- so sad
+
+## intent:bot_challenge
+- are you a bot?
+- are you a human?
+- am I talking to a bot?
+- am I talking to a human?
+
+## intent:play_music
+- play the song [So far away](song)
+- please play [Let me down slowly](song)
+- please play [Faded](song)
+- I wanna listen to [Come thru](song)
+- play [Fly away](song) please
+- I want to listen to music
+- Bật nhạc cho tớ nghe đi
+- Tớ muốn nghe nhạc
+- let me listen to [All falls down](song)
+- I want the song [Girls like you](song)
+- the song [What are words](song) please
+- Can you play [Thousand year](song)?
+
