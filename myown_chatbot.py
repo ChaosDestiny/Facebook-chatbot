190aa1f97277df8e582757f3142d41ee528f2407
Tran Dinh Hung 20170078 <hung.td170078@sis.hust.edu.vn>
2020-05-11 21:01:44 +0700

    deploy chatbot

--- myown_chatbot.py
+++ myown_chatbot.py
@@ -1,8 +1,12 @@
 import os
 from rasa.core.channels.rasa_chat import RasaChatInput
+from rasa.core.channels.channel import CollectingOutputChannel, UserMessage
 from rasa.core.agent import Agent
 from rasa.core.interpreter import RasaNLUInterpreter
 from rasa.core.utils import EndpointConfig
+from rasa.core import utils
+from flask import render_template, Blueprint, jsonify, request
+from sanic import response
 
 # load your trained agent
 interpreter = RasaNLUInterpreter("models/nlu/")
@@ -11,14 +15,43 @@ action_endpoint = EndpointConfig(url="https://lunachatbot-prj2-actions.herokuapp
 agent = Agent.load(MODEL_PATH, interpreter=interpreter, action_endpoint=action_endpoint)
 
 class MyNewInput(RasaChatInput):
+    @classmethod
+    def name(cls):
+        return "rasa"
+    
     def _check_token(self, token):
         if token == 'jackfrost':
             return {'username': 1234}
         else:
-            print("Fail to check token: {}.".format(token))
+            print("Failed to check token: {}.".format(token))
             return None
         
-input_channel = MyNewInput(url='https://lunachatbot-prj2.herokuapp.com')
+    def blueprint(self, on_new_message):
+        templates_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'myown_chatbot')
+        custom_webhook = Blueprint('custom_webhook', __name__, template_folder = templates_folder)  
+        
+        @custom_webhook.route("/", methods=['GET'])
+        def health():
+            return jsonify({"status": "ok"})
+        
+        @custom_webhook.route("/chat", methods=['GET'])
+        def chat():
+            return render_template('index.html')
+        
+        @custom_webhook.route("/webhook", methods=['POST'])
+        def receive():
+            sender_id = self._extract_sender(request)
+            text = self._extract_message(request)
+            should_use_stream = utils.bool_arg("stream", default=False)
+            if should_use_stream:
+                return response.stream(self.stream_response(on_new_message, text, sender_id), content_type='text/event-stream')
+            else:
+                collector = CollectingOutputChannel()
+                on_new_message(UserMessage(text, collector, sender_id))
+                return jsonify(collector.messages)
+            
+        return custom_webhook        
 
+input_channel = MyNewInput(url='https://lunachatbot-prj2.herokuapp.com')
 # set serve_forever=False if you want to keep the server running
 s = agent.handle_channels([input_channel], int(os.environ.get('PORT',5004)))
