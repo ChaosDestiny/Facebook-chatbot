a873b6805d47327fb03eb0bac7f26916fd927702
Tran Dinh Hung 20170078 <hung.td170078@sis.hust.edu.vn>
2020-05-11 22:01:22 +0700

    deploy chatbot

--- myown_chatbot.py
+++ myown_chatbot.py
@@ -1,6 +1,6 @@
 import os
 from rasa.core.channels.rasa_chat import RasaChatInput
-from rasa.core.channels.channel import CollectingOutputChannel, UserMessage
+from rasa.core.channels.channel import CollectingOutputChannel, UserMessage, RestInput
 from rasa.core.agent import Agent
 from rasa.core.interpreter import RasaNLUInterpreter
 from rasa.core.utils import EndpointConfig
@@ -14,10 +14,10 @@ MODEL_PATH = "models/20200511-190147.tar.gz"
 action_endpoint = EndpointConfig(url="https://lunachatbot-prj2-actions.herokuapp.com/webhook")
 agent = Agent.load(MODEL_PATH, interpreter=interpreter, action_endpoint=action_endpoint)
 
-class MyNewInput(RasaChatInput):
+class MyNewInput(RestInput):
     @classmethod
     def name(cls):
-        return "rasa"
+        return "rest"
     
     def _check_token(self, token):
         if token == 'jackfrost':
